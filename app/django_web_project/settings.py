66ed6bd3a41564cb337ab176d0863adfe5abae93
Vajk Turi <turi.vajk@gmail.com>
2019-11-30 02:00:46 +0100

    Addedn nginx webserver
    Signed-off-by: Vajk Turi <turi.vajk@gmail.com>

--- app/django_web_project/settings.py
+++ app/django_web_project/settings.py
@@ -149,7 +149,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+STATIC_URL = "/staticfiles/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Email messages from the site
@@ -168,6 +174,3 @@ STATIC_URL = '/static/'
 # For development
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-WAGTAIL_SITE_NAME = 'My Example Site'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
