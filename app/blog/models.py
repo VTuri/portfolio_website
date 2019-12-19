7b70b94fd404a2ae43f29287cca24f51437c51da
Vajk Turi <turi.vajk@gmail.com>
2019-12-19 18:18:16 +0100

    models updated to show the correct name instead of "object"
    Signed-off-by: Vajk Turi <turi.vajk@gmail.com>

--- app/blog/models.py
+++ app/blog/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -13,7 +16,10 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField('Category', related_name='posts')
-    image_path = models.CharField(max_length=30, default="")
+    image_path = models.CharField(max_length=30, default="img/default_img.png")
+
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
--- app/projects/models.py
+++ app/projects/models.py
@@ -7,4 +7,7 @@ class Project(models.Model):
     description = models.TextField(default="")
     body = RichTextUploadingField(default="")
     technology = models.CharField(max_length=20)
-    image_path = models.CharField(max_length=30)
+    image_path = models.CharField(max_length=30, default="img/default_img.png")
+
+    def __str__(self):
+        return self.title
