1fcf9526651e43a351b6851ccd653abca7dee176
Vajk Turi <turi.vajk@gmail.com>
2020-02-01 10:37:04 +0100

    Whatson scraper finished
    Signed-off-by: Vajk Turi <turi.vajk@gmail.com>

--- app/whatson_scrape/forms.py
+++ app/whatson_scrape/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class SearchForm(forms.Form):
-    number_of_results = forms.IntegerField(required=True, label="Number")
-    from_date = forms.CharField(required=True, label="From")
-    to_date = forms.CharField(required=True, label="To")
+    number_of_results = forms.IntegerField(required=True, label="Maximum number of results")
+    from_date = forms.CharField(required=True, label="Start date (Example: 01/02/2020)")
+    to_date = forms.CharField(required=True, label="End date (Example: 10/02/2020)")
