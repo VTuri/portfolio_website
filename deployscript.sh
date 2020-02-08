9e659a2ef62932238b08aef9d3a879780203e41b
Vajk Turi <turi.vajk@gmail.com>
2020-02-08 22:45:14 +0100

    GitLab ci integration
    Signed-off-by: Vajk Turi <turi.vajk@gmail.com>

---
+++ deployscript.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+cd git
+
+# Check if the repo still exists in the folder, clone if it isn't
+DIR="portfolio_website"
+if [ -d "$DIR" ]; then
+  echo "${DIR} already exists."
+else
+  echo "Cloning repository"
+  git clone https://github.com/VTuri/portfolio_website.git
+fi
+
+cd ${DIR}
+
+# Get the newest version and restart the containers
+git pull
+docker-compose -f docker-compose.prod.yml up -d --build
+docker-compose -f docker-compose.prod.yml exec web python manage.py collectstatic --no-input --clear
+
+exit
