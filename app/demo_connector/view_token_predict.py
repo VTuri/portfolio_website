cd231f0612832a97be85871e05906b3c527ff77f
Vajk Turi <turi.vajk@gmail.com>
2020-03-02 21:05:45 +0100

    working data flow
    Signed-off-by: Vajk Turi <turi.vajk@gmail.com>

--- app/demo_connector/view_token_predict.py
+++ app/demo_connector/view_token_predict.py
@@ -1,5 +1,35 @@
+import requests
 from django.shortcuts import render
+from django.template import RequestContext
+
+from .forms import TokenPredictForm
 
 
 def token_predict(request):
-    return render(request, "token_predict.html")
+    if request == 'GET':
+        form = TokenPredictForm()
+        args = {
+            "form": form
+        }
+    else:
+        form = TokenPredictForm(request.POST)
+        if form.is_valid():
+            number_of_results = form.cleaned_data['forecast']
+
+            response = requests.get(
+                # Container port is needed not the "host port" from the compose
+                "http://token_predict:5000/predict/?forecast={}".format(number_of_results),
+                verify=False,
+                params=request.GET)
+
+            data = response.json()
+            args = {
+                "data": data,
+                "form": form
+            }
+        else:
+            args = {
+                "form": form
+            }
+
+    return render(request, "token_predict.html", args, RequestContext(request))
