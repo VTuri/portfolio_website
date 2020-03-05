7e07b18c3ca574191625ec705a3fc600042d2794
Vajk Turi <turi.vajk@gmail.com>
2020-03-05 08:58:40 +0100

    Update views.py

--- app/home/views.py
+++ app/home/views.py
@@ -19,7 +19,8 @@ def home_index(request):
 
 
 def handler404(request, exception, template_name="404.html"):
-    response = render("404.html")
+    response = render("404.html", {},
+                                  context_instance=RequestContext(request))
     response.status_code = 404
     return response
 
